2781929667af9a4568f3513740955d0137669322
N Chaitanya Srikar <ncsrikar@gmail.com>
2020-06-26 22:50:30 +0530

    Removed sqlite3

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "b'A\xb4\t\xa3~\x16\xfb\xc3\x8f\xe1l\\V\x96\xfd\xaf'"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+
