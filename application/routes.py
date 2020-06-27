1bbf57b7e360375c8c31ec76f88bbf547ca1ca4a
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2020-06-27 18:49:37 +0530

    Made changes to footer, navbar and other areas and added basic routes

--- application/routes.py
+++ application/routes.py
@@ -17,8 +17,8 @@ def login():
 
     if(form.validate_on_submit()):
         user = Login_details.query.filter
+        account = True
         if(account):
-            flash("Logged in Successfully","success")
             session['email'] = request.form.get("email")
             return redirect("/") 
         else:
@@ -31,32 +31,27 @@ def logout():
 
 @app.route("/register")
 def register():
-    
     return render_template("register.html",register=True)
-@app.route("/enrollment",methods = ["GET","POST"])
-def enrollment():
+
+@app.route("/patient")
+def patient():
     if(session.get('email')):
-        id = request.form.get("courseID")
-        title = request.form.get("title")
-        term = request.form.get("term")
-        print(id,title,term)
-        if(id):
-            cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cur.execute('SELECT * FROM `customer`.`enrollment` WHERE email = %s AND courseid = %s', (session.get('email'),id))
-            account = cur.fetchone()
-            if(account):
-                flash("Looks like you have already registered","danger")
-                return redirect("/courses")
-            else:
-                cur.execute("INSERT INTO `customer`.`enrollment` (`Sno`,`email`, `courseid`, `coursetitle`, `term`) VALUES (%s, %s, %s, %s)",(0,session.get('email'),id, title,term))
-                mysql.connection.commit() 
-            return render_template("enrollment.html",enrollment=True,id=id,title=title,term=term,selectedcourses = None)
-        else:
-            cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cur.execute('SELECT * FROM `customer`.`enrollment`')
-            alldata = cur.fetchall()
-            print(alldata)
-            return render_template("enrollment.html",enrollment=True,id=id,title=title,term=term,selectedcourses = alldata)
+        return render_template("index.html", login= False, patient=True,loggedin = session.get('email'))
+    else:
+        flash("looks like you are not logged in! Please log in","danger")
+        return redirect("/login")
+
+@app.route("/medicines")
+def medicines():
+    if(session.get('email')):
+        return render_template("index.html", login= False, medicines=True,loggedin = session.get('email'))
+    else:
+        flash("looks like you are not logged in! Please log in","danger")
+        return redirect("/login")
+@app.route("/diagnostics")
+def diagnostics():
+    if(session.get('email')):
+        return render_template("index.html", login= False, diagnostics=True,loggedin = session.get('email'))
     else:
         flash("looks like you are not logged in! Please log in","danger")
         return redirect("/login")
