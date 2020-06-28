f1597f2ffc13ac0022f9232659f75530c3eba825
N Chaitanya Srikar <ncsrikar@gmail.com>
2020-06-28 20:54:17 +0530

    Login and Registration implemented

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,IntegerField,DateTimeField,SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -7,3 +7,14 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password",validators=[DataRequired()])
     rememberme = BooleanField("rememberme")
     submit = SubmitField("login")
+
+class Register(FlaskForm):
+    patient_ssn_id = IntegerField("Patient SSN ID", validators=[DataRequired()])
+    name = StringField("Name",validators=[DataRequired()])
+    age = IntegerField("Age", validators=[DataRequired()])
+    date = DateTimeField("Date",validators=[DataRequired()],format='%Y-%m-%dT%H:%M')
+    type = SelectField("Type", choices=[(' General ward',' General ward'),(' Semi Sharing',' Semi Sharing'),('Single Room','Single Room')])
+    address = StringField("Address",validators=[DataRequired()])
+    city = StringField("City",validators=[DataRequired()])
+    state = StringField("State",validators=[DataRequired()])
+    submit = SubmitField("register")
--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 import flask
 from application import db
+from datetime import datetime
 print(db)
 class login_details(db.Model):
     __tablename__ = 'login_details'
@@ -7,4 +8,17 @@ class login_details(db.Model):
     email = db.Column(db.String(255),unique=True)
     password = db.Column(db.String(255))
     accesslevel = db.Column(db.Integer)
+class Patient(db.Model):
+    __tablename__ = 'patient_details'
+    patient_id = db.Column(db.Integer, unique = True)
+    patient_ssn = db.Column(db.Integer, primary_key = True)
+    patient_name = db.Column(db.String(255))
+    patient_address= db.Column(db.String(1000))
+    patient_city =  db.Column(db.String(255))
+    patient_state =  db.Column(db.String(255))
+    patient_age = db.Column(db.Integer)
+    patient_doj = db.Column(db.DateTime, default = datetime.now)
+    patient_rtype = db.Column(db.String(255))
+    patient_status = db.Column(db.String(255))
+    
     
